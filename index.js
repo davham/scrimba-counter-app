efd6279b70dc8668ed6e83273ac45908054cf8bf
David Hamilton <david.hamilton5@gmail.com>
2023-01-28 20:12:48 -0500

    Built a counter app in javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let resetEl = document.getElementById("reset-el")
+let count = 0
+
+
+function add(){
+    count +=  1
+    countEl.textContent = count    
+}
+
+function save(){
+    saveEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
+}
+function reset(){
+    countEl.textContent = 0
+    saveEl.textContent = "Previous entries: "
+    
+}
+
